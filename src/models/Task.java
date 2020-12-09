73271012158569b4a06f1cb0ad0a57cbfc13d09d
misaki.tomita <misakitomita@misakinoMacBook-Pro.local>
2020-12-09 20:00:29 +0900

    Add databasefiles

---
+++ src/models/Task.java
@@ -0,0 +1,69 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+
+public class Task {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    //以下３項目、全て入力必須項目(nullable = false)
+    //作成日時：日付型
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    //更新日時：日付型
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+
+    //タスクの内容：文字列型
+    @Column(name = "content", nullable = false)
+    private String content;
+
+
+    //getter及びsetter------------------------------------------
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
